23881652874d2221eed9290caa5418fcb4055635
João Ferreira <joao.bento.ramos.ferreira@gmail.com>
2023-06-25 23:19:43 -0300

    25/06

--- auto_bolsa/buca_web.py
+++ auto_bolsa/buca_web.py
@@ -8,20 +8,14 @@ def busca(vl):
     dr = webdriver.Chrome()
     dr.get('https://finance.yahoo.com/')
     pyautogui.click(332, 196, duration=1)
-    dr.find_element(By.XPATH, '//*[@id="yfin-usr-qry"]').send_keys(f'{val}')
+    pyautogui.typewrite(f'{vl}')
     pyautogui.click(589, 193, duration=1)
-    pyautogui.click(430, 663, duration=1)
-    pyautogui.click(1045, 683, duration=1)
-    # apartir da aqui é melhor usar o pyautogui 7
-
-
-    sleep(2)
-    '''dr.find_element(By.XPATH, '//*[@id="header-desktop-search-button"]').click()
-    dr.find_element(By.XPATH, '//*[@id="quote-nav"]/ul/li[5]/a/span').click()'''
-    sleep(100)
-    # os valores fica diferente para cada etf
-    dr.find_element(By.CLASS_NAME, '').click()
+    pyautogui.click(430, 663, duration=15)
+    for c in range(0, 6):
+        pyautogui.click(1045, 683, duration=1)
+    pyautogui.click(646, 660, duration=10)
     sleep(10)
     dr.close()
+
 
 busca("DIVO11")
